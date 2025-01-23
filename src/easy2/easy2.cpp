bacead5c1ad7914433250159ed54ebf036101df6
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-01-23 15:58:12 +0100

    done easy2.cpp

--- src/easy2/easy2.cpp
+++ src/easy2/easy2.cpp
@@ -1,9 +1,21 @@
-// https://training.olinfo.it/task/easy2
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-        return EXIT_SUCCESS;
+        ifstream cin("input.txt");
+        ofstream cout("output.txt");
+
+        int N,A,B,ris=-1; cin>>N;
+
+        int V[N];
+
+        for(int i=0; i<N+1; i++)
+        {
+                cin>>A>>B;
+                if((A+B)%2==0)
+                        ris = max(ris,A+B);
+        }
+        cout<<ris;
 }
