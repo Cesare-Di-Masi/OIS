d5c2a98efc5bdbbc04825f39563d053460c0e63f
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-02-10 13:56:27 +0100

    starting via emilea

--- src/oii_aemilia/grader.cpp
+++ src/oii_aemilia/grader.cpp
@@ -1,18 +1,54 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<int> verifica(int N, vector<int> T);
 
 int main() {
     int N;
+    int l=2e9, s,counter=0;
     cin >> N;
 
     vector<int> T(N);
+    vector<int> Z(N);
     for (int i = 0; i < N; i++)
+    {
         cin >> T[i];
+        if(T[i]<l)
+        {   
+            l=T[i];
+            s=i;
+        }
+    }
 
-    vector<int> P = verifica(N, T);
+    int e=s;
+
+    Z[0] = s;
+
+    while(counter<N)
+    {
+
+        if(T[e+1]==T[e])
+        {
+            Z[counter] = e+1;
+            counter++;
+            e++;
+        }
+        if(T[s-1]== T[s])
+        {
+            Z[counter] = s-1;
+            counter++;
+            s--;
+        }
+
+        if(T[e+1] == l+1)
+        {
+            Z[counter] = e+1;
+            counter++;
+            
+        } 
+
+    }
+
+
+
     
     cout << P.size() << '\n';
     for (int i = 0; i < P.size(); i++)
