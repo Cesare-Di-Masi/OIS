5458093c0c317294ef3fd8faabd6d7e6e37416c6
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-02-18 10:38:09 +0100

    something, IDK

--- src/preoii_dante/dante.cpp
+++ src/preoii_dante/dante.cpp
@@ -1,7 +1,29 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int rimembra(int N, int K, vector<int> V){
-    return 69;
+int rimembra(int N, int K, vector<int> V)
+{
+    
 }
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int N,K; cin>>N>>K;
+
+    vector<int> V(N);
+
+    for(int i=0; i<N; i++)
+    {
+        cin>>V[i];
+    }
+
+    cout<<rimembra(N,K,V);
+
+
+}
+
+
---
+++ src/soldatini/soldatini.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int t) {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    int risposta = 0;
+
+    cout << "Case #" << t << ": " << risposta << "\n";
+}
+
+int main() {
+    
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; t++) {
+        solve(t);
+    }
+
+    return 0;
+}
